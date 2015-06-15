5c64a3512b5873a2bf3a36565c8b53de8dd557fb
CassianoNeves <cassianodasnevesc10@gmail.com>
2015-06-15 17:01:30 -0300

    create(CRUD Skill)

---
+++ src/main/java/whiskill/model/Skill.java
@@ -0,0 +1,49 @@
+package whiskill.model;
+
+public class Skill {
+	
+	private int idSkill;
+	private String nome;
+	private String descricao;
+	private Trilha trilha;
+	
+	public Skill(){}
+	
+	public Skill( int idSkill, String nome, String descricao ){
+		this.idSkill = idSkill;
+		this.nome = nome;
+		this.descricao = descricao;
+	}
+		
+	public int getIdSkill() {
+		return idSkill;
+	}
+	
+	public void setIdSkill(int idSkill) {
+		this.idSkill = idSkill;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public Trilha getTrilha() {
+		return trilha;
+	}
+
+	public void setTrilha(Trilha trilha) {
+		this.trilha = trilha;
+	}
+}
