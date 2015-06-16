46dac21d1c20d73372830c87c712bc43317655f3
Gabriel Moutinho <gabriel.moutinho@live.com>
2015-06-16 10:52:55 -0300

    feat(ProjetoController): atualizações no Controller para crud integrado com Skills

--- src/main/java/whiskill/controller/ProjetoController.java
+++ src/main/java/whiskill/controller/ProjetoController.java
@@ -7,9 +7,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import whiskill.dao.ProjetoDao;
 import whiskill.dao.SkillDao;
+import whiskill.dao.TrilhaDao;
+import whiskill.model.Colaborador;
 import whiskill.model.Projeto;
 
 @Controller
@@ -19,21 +22,31 @@ public class ProjetoController {
 	ProjetoDao projetoDao;
 	@Inject
 	SkillDao skillDao;
+	@Inject
+	TrilhaDao trilhaDao;
+	
 
 	@RequestMapping( value = "/projeto/cadastro", method = RequestMethod.GET )
 	public String projetoCadastro(Model model){
-		model.addAttribute("skills", skillDao.buscarTodasSkills() );
+		model.addAttribute( "skills", skillDao.buscarTodasSkills() );
+		model.addAttribute( "trilhas", trilhaDao.buscaTodasTrilhas() );		
 		return "/projeto/ProjetoCadastro";
 	}
-
-	@RequestMapping( value = "/projeto/inserir", method = RequestMethod.POST )
-	public String projetoInserir( Projeto projeto , @RequestParam int idSkill ){
-		projetoDao.inserirProjeto( projeto );
-		int idProjeto = projetoDao.buscaIdDoProjetoPorNome(projeto.getNome());
-		projetoDao.inserirSkillProjeto(idProjeto, idSkill);
-		return "redirect:/projeto/listar";
+	@ResponseBody
+	@RequestMapping( value = "/projeto/inserir/projeto", method = RequestMethod.POST )
+	public int projetoInserir( Projeto projeto ){
+		int idCriado = projetoDao.inserirProjeto( projeto );
+		return idCriado;
 	}
-
+	
+	@ResponseBody
+	@RequestMapping( value = "/projeto/inserir/SkillProjeto", method = RequestMethod.POST )
+	public String projetoInserirSkillProjeto(  @RequestParam String query ){
+		 projetoDao.inserirSkillProjeto( query );
+		return "ok";
+	}
+	
+	
 	@RequestMapping( value = "/projeto/listar", method = RequestMethod.GET )
 	public String projetoListar( Model model ){
 		model.addAttribute( "projetos", projetoDao.buscaTodosProjetos() );
@@ -43,21 +56,29 @@ public class ProjetoController {
 	@RequestMapping( value = "/projeto/atualizar", method = RequestMethod.GET )
 	public String projetoAtualizar( Model model, @RequestParam int idProjeto){
 		model.addAttribute( "projeto", projetoDao.buscaProjetoPorId( idProjeto ) );
-		model.addAttribute("skills", skillDao.buscarTodasSkills() );
+		model.addAttribute( "skillsDoProjeto", projetoDao.buscarSkillsProjeto( idProjeto ) );
+		model.addAttribute( "skills", skillDao.buscarTodasSkills() );
+		model.addAttribute( "trilhas", trilhaDao.buscaTodasTrilhas() );
 		return "/projeto/ProjetoAtualizar";
 	}
-
+	
+	@ResponseBody
 	@RequestMapping( value = "/projeto/atualizar", method = RequestMethod.POST )
-	public String projetoAtualizar( Projeto projeto, @RequestParam int idSkill ){
+	public String projetoAtualizar( Projeto projeto){
 		projetoDao.atualizarProjeto( projeto );
-		projetoDao.inserirSkillProjeto(projeto.getIdProjeto(), idSkill);
-		return "redirect:/projeto/listar";
+		return "ok";
 	}
 
 	@RequestMapping( value = "/projeto/excluir", method = RequestMethod.GET )
 	public String projetoExcluir( @RequestParam int idProjeto ){
 		projetoDao.excluirProjeto( idProjeto);
 		return "redirect:/projeto/listar";
-
 	}	
+
+	@ResponseBody
+	@RequestMapping( value = "/projeto/excluirSkills", method = RequestMethod.POST )
+	public String colaboradorExcluirSkills( @RequestParam int idProjeto ){
+		projetoDao.excluirSkillsProjeto( idProjeto );
+		return "ok";
+	}
 }
