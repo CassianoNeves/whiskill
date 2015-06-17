b794ad9795ad840f133b9314f1f961316a552c44
CassianoNeves <cassianodasnevesc10@gmail.com>
2015-06-17 00:06:26 -0300

    Criando metodo para buscar colaboradores do projeto

--- src/main/java/whiskill/dao/ProjetoColaboradorDao.java
+++ src/main/java/whiskill/dao/ProjetoColaboradorDao.java
@@ -1,9 +1,14 @@
 package whiskill.dao;
 
+
+import java.util.List;
+
 import javax.inject.Inject;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
+
+import whiskill.model.Colaborador;
 
 @Component
 public class ProjetoColaboradorDao {
@@ -11,5 +16,11 @@ public class ProjetoColaboradorDao {
 	@Inject
 	private JdbcTemplate jdbcTemplate;
 	
-	
+//	public List<Colaborador> bucarColaboradoresPorIdDoProjeto( int idProjeto ){
+//		jdbcTemplate.query( "SELECT C.* FROM PROJETOCOLABORADOR PC "
+//				+ "JOIN COLABORADOR C ON C.IDCOLABORADOR = PC.IDCOLABORADOR  "
+//				+ "WHERE IDPROJETO = ?", rse)
+//		
+//		return null;
+//	}
 }
